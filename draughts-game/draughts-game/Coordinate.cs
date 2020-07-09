a0554897192ab106b0ac8a8c5bfac6d8e79e606c
Henrique Andrade <henrique.andrade22@hotmail.com>
2020-07-09 15:25:30 -0300

    Create coordinate class

---
+++ draughts-game/draughts-game/Coordinate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace draughts_game
+{
+    class Coordinate
+    {
+        private readonly int x;
+        private readonly int y;
+
+        public Coordinate(string coordinate)
+        {
+            string[] split = coordinate.Split(',');
+
+            if (split.Length is 2)
+            {
+                if (!int.TryParse(split[0], out x))
+                {
+                    throw new Exception();
+                }
+
+                if (!int.TryParse(split[1], out y))
+                {
+                    throw new Exception();
+                }
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        
+        public int X { get => x; }
+        
+        public int Y { get => y; }
+    }
+}
