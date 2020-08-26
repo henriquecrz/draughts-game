01dd601283aa0a063334755d229bccca5ce9327a
Henrique Andrade <henrique.andrade22@hotmail.com>
2020-08-25 21:59:01 -0300

    Comment PlayMenu class

--- src/DraughtsGame/UserInterface/PlayMenu.cs
+++ src/DraughtsGame/UserInterface/PlayMenu.cs
@@ -4,61 +4,61 @@ namespace DraughtsGame.UserInterface
 {
     public static class PlayMenu
     {
-        private static bool _quit = false;
+        // private static bool _quit = false;
 
-        public static void Show()
-        {
-            do
-            {
-                if (Game.Players.Count != 0)
-                {
-                    Game.ShowPlayers();
-                }
+        // public static void Show()
+        // {
+        //     do
+        //     {
+        //         if (Game.Players.Count != 0)
+        //         {
+        //             Game.ShowPlayers();
+        //         }
 
-                if (Game.Players.Count < Constant.NUMBER_OF_PLAYERS_REQUIRED)
-                {
-                    Console.WriteLine("First, we need to create at least {0} players;", Constant.NUMBER_OF_PLAYERS_REQUIRED);
+        //         if (Game.Players.Count < Constant.NUMBER_OF_PLAYERS_REQUIRED)
+        //         {
+        //             Console.WriteLine("First, we need to create at least {0} players;", Constant.NUMBER_OF_PLAYERS_REQUIRED);
 
+        //             Game.RunOverPlayers();
+        //         }
+        //         else
+        //         {
+        //             Console.WriteLine("Select {0} players to start a new match.");
+        //         }
 
-                }
-                else
-                {
-                    Console.WriteLine("Select {0} players to start a new match.");
-                }
+        //         Console.Clear();
+        //         Console.WriteLine("#==========================#");
+        //         Console.WriteLine("#                          #");
+        //         Console.WriteLine("#==========================#");
+        //         Console.WriteLine();
+        //         Console.WriteLine("Type \"Play\" or \"Quit\".");
+        //         Console.Write(">> What would you like to do? ");
 
-                Console.Clear();
-                Console.WriteLine("#==========================#");
-                Console.WriteLine("#                          #");
-                Console.WriteLine("#==========================#");
-                Console.WriteLine();
-                Console.WriteLine("Type \"Play\" or \"Quit\".");
-                Console.Write(">> What would you like to do? ");
+        //         var command = Console.ReadLine().Trim().ToLower();
 
-                var command = Console.ReadLine().Trim().ToLower();
+        //         CommandRoute(command);
+        //     } while (!_quit);
 
-                CommandRoute(command);
-            } while (!_quit);
+        //     Console.WriteLine("See you soon!");
+        //     Console.Read();
+        // }
 
-            Console.WriteLine("See you soon!");
-            Console.Read();
-        }
+        // private static void CommandRoute(string command)
+        // {
+        //     switch (true)
+        //     {
+        //         case true when command.StartsWith(Command.CREATE):
+        //             {
 
-        private static void CommandRoute(string command)
-        {
-            switch (true)
-            {
-                case true when command.StartsWith(Command.CREATE):
-                    {
-
-                        break;
-                    }
-                default:
-                    {
-                        Console.WriteLine(Command.NOT_RECOGNIZED, command);
-                        break;
-                    }
-            }
-        }
+        //                 break;
+        //             }
+        //         default:
+        //             {
+        //                 Console.WriteLine(Command.NOT_RECOGNIZED, command);
+        //                 break;
+        //             }
+        //     }
+        // }
 
         // public static void CreatePlayer()
         // {
