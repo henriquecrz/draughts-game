2112c1df1588fd5a21c08248d438034202549ce8
Henrique Andrade <henrique.andrade22@hotmail.com>
2020-09-20 11:49:20 -0300

    Try to implement escape

--- src/DraughtsGame/Program.cs
+++ src/DraughtsGame/Program.cs
@@ -35,39 +35,41 @@ namespace DraughtsGame
 
             // System.Console.WriteLine("Deu certo");
 
+            //https://www.c-sharpcorner.com/blogs/difference-between-readline-read-reakkey-in-c-sharp
             Console.Clear();
             Console.Write("Enter your name and press ENTER.  (ESC to cancel): ");
-            string name = readLineWithCancel();
+            Console.ReadLine();
+            // string name = readLineWithCancel();
 
-            Console.WriteLine("\r\n{0}", name == null ? "Cancelled" : name);
+            // Console.WriteLine("\r\n{0}", name == null ? "Cancelled" : name);
 
             Console.ReadLine();
         }
 
         //Returns null if ESC key pressed during input.
-        private static string readLineWithCancel()
-        {
-            string result = null;
+        // private static string readLineWithCancel()
+        // {
+        //     string result = null;
 
-            StringBuilder buffer = new StringBuilder();
+        //     StringBuilder buffer = new StringBuilder();
 
-            //The key is read passing true for the intercept argument to prevent
-            //any characters from displaying when the Escape key is pressed.
-            ConsoleKeyInfo info = Console.ReadKey(true);
-            while (info.Key != ConsoleKey.Enter && info.Key != ConsoleKey.Escape)
-            {
-                Console.Write(info.KeyChar);
-                buffer.Append(info.KeyChar);
-                info = Console.ReadKey(true);
-            }
+        //     //The key is read passing true for the intercept argument to prevent
+        //     //any characters from displaying when the Escape key is pressed.
+        //     ConsoleKeyInfo info = Console.ReadKey(true);
+        //     while (info.Key != ConsoleKey.Enter && info.Key != ConsoleKey.Escape)
+        //     {
+        //         Console.Write(info.KeyChar);
+        //         buffer.Append(info.KeyChar);
+        //         info = Console.ReadKey(true);
+        //     }
 
-            if (info.Key == ConsoleKey.Enter)
-            {
-                result = buffer.ToString();
-            }
+        //     if (info.Key == ConsoleKey.Enter)
+        //     {
+        //         result = buffer.ToString();
+        //     }
 
-            return result;
-        }
+        //     return result;
+        // }
 
         private static void SetConsoleConfiguration()
         {
