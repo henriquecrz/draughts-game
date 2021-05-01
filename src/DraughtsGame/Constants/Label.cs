6fb31959aa643ce898f9923dba86506ff0165678
Henrique Andrade <henrique.andrade22@hotmail.com>
2021-04-30 23:15:48 -0300

    Refact startup menu

--- src/DraughtsGame/Constants/Label.cs
+++ src/DraughtsGame/Constants/Label.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Text;
+
 namespace DraughtsGame.Constants
 {
     public class Label
@@ -5,5 +8,24 @@ namespace DraughtsGame.Constants
         public const string NAME_INPUT = "Insert your user name: ";
 
         public const string CHAR_INPUT = "Insert a char you'll use. Remember, you can only use one letter: ";
+
+        static Label()
+        {
+            FillTitle();
+        }
+
+        public static string Title { get; private set; }
+
+        private static void FillTitle()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("#==========================#");
+            stringBuilder.Append(Environment.NewLine + "# Henrique's Draughts Game #");
+            stringBuilder.Append(Environment.NewLine + "#==========================#");
+            stringBuilder.Append(Environment.NewLine);
+
+            Title = stringBuilder.ToString();
+        }
     }
 }
--- src/DraughtsGame/Program.cs
+++ src/DraughtsGame/Program.cs
@@ -1,84 +1,26 @@
 ﻿using System;
-using System.Text;
-using System.Threading;
 using DraughtsGame.UserInterface;
 
 namespace DraughtsGame
 {
     public class Program
     {
-        static void Main()
+        public static void Main()
         {
             SetConsoleConfiguration();
-            // StartupMenu.Show();
-
-            //int cont = 0; control flow by a counter
-            //Console.ReadLine() catch ConsoleKey.Escape
-
-            // do
-            // {
-            //     System.Console.Write("Label: ");
-            //     var a = Console.ReadLine();
-            // } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
-
-            // do
-            // {
-            //     if (!Console.KeyAvailable)
-            //     {
-            //         System.Console.Write("Label: ");
-            //         string a = Console.ReadLine();
-            //     }
-
-            //     Console.WriteLine("Passou");
-            //     break;
-            // } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
-
-            // System.Console.WriteLine("Deu certo");
-
-            //https://www.c-sharpcorner.com/blogs/difference-between-readline-read-reakkey-in-c-sharp
-            Console.Clear();
-            Console.Write("Enter your name and press ENTER.  (ESC to cancel): ");
-            Console.ReadLine();
-            // string name = readLineWithCancel();
-
-            // Console.WriteLine("\r\n{0}", name == null ? "Cancelled" : name);
-
-            Console.ReadLine();
+            StartupMenu.Show();
+            ResetConsoleConfiguration();
         }
-
-        //Returns null if ESC key pressed during input.
-        // private static string readLineWithCancel()
-        // {
-        //     string result = null;
-
-        //     StringBuilder buffer = new StringBuilder();
-
-        //     //The key is read passing true for the intercept argument to prevent
-        //     //any characters from displaying when the Escape key is pressed.
-        //     ConsoleKeyInfo info = Console.ReadKey(true);
-        //     while (info.Key != ConsoleKey.Enter && info.Key != ConsoleKey.Escape)
-        //     {
-        //         Console.Write(info.KeyChar);
-        //         buffer.Append(info.KeyChar);
-        //         info = Console.ReadKey(true);
-        //     }
-
-        //     if (info.Key == ConsoleKey.Enter)
-        //     {
-        //         result = buffer.ToString();
-        //     }
-
-        //     return result;
-        // }
 
         private static void SetConsoleConfiguration()
         {
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.CancelKeyPress += (sender, args) =>
-            {
-                Util.Exit();
-            };
+        }
+
+        private static void ResetConsoleConfiguration()
+        {
+            Console.ResetColor();
         }
     }
 }
